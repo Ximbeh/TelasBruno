826a2fdd913bc2d013a00da49d192388f4b4c946
vanysai <jaizamsf@gmail.com>
2023-05-05 20:48:22 -0300

    telas um

--- App.js
+++ App.js
@@ -1,11 +1,100 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+// import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+// import TelaB from './screens/TelaB.js';
+
+// export default function App() {
+//   const handlePress = () => {
+//     console.log('Botão pressionado!');
+//   };
+
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<PaginaA />} />
+//         <Route path="/tela-b" element={<TelaB />} />
+//         <Route path="/tela-c" element={<TelaC />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// function PaginaA() {
+//   const handlePress = () => {
+//     console.log('Botão pressionado!');
+//   };
+
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.text}>Tela A</Text>
+//       <Link to="/tela-b">
+//         <TouchableOpacity style={styles.button} onPress={handlePress}>
+//           <Text style={styles.buttonText}>Ir para a Tela B</Text>
+//         </TouchableOpacity>
+//       </Link>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   text: {
+//     fontSize: 24,
+//     marginBottom: 20,
+//   },
+//   button: {
+//     backgroundColor: 'purple',
+//     padding: 10,
+//     borderRadius: 5,
+//   },
+//   buttonText: {
+//     color: '#fff',
+//     fontSize: 16,
+//   },
+// });
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+import TelaB from './screens/TelaB.js';
+import TelaC from './screens/TelaC.js';
 
 export default function App() {
+  const handlePress = () => {
+    console.log('Botão pressionado!');
+  };
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<PaginaA />} />
+        <Route path="/tela-b" element={<TelaB />} />
+        <Route path="/tela-c" element={<TelaC />} />
+      </Routes>
+    </Router>
+  );
+}
+
+function PaginaA() {
+  const handlePress = () => {
+    console.log('Botão pressionado!');
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Tela 1</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Tela A</Text>
+      <Link to="/tela-b">
+        <TouchableOpacity style={styles.button} onPress={handlePress}>
+          <Text style={styles.buttonText}>Ir para a Tela B</Text>
+        </TouchableOpacity>
+      </Link>
     </View>
   );
 }
@@ -16,5 +105,19 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: 'green',
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
   },
 });
