36379affcd98ba6c552acc5bc070adc48a04125c
anabmartins <ana_beatriz-martins@estudante.sesisenai.org.br>
2023-05-06 19:28:35 -0300

    tela 3 Bottom Tabs Navigator

--- tela3/App.js
+++ tela3/App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import TelaA from './screens/TelaA';
+import TelaB from './screens/TelaB';
+import TelaC from './screens/TelaC';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'TelaA') {
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+            } else if (route.name === 'TelaB') {
+              iconName = focused ? 'ios-person' : 'ios-person-outline';
+            } else if (route.name === 'TelaC') {
+              iconName = focused ? 'ios-chatbubbles' : 'ios-chatbubbles-outline';
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: '#ff33cc',
+          inactiveTintColor: 'gray',
+        }}
+      >
+        <Tab.Screen name="TelaA" component={TelaA} />
+        <Tab.Screen name="TelaB" component={TelaB} />
+        <Tab.Screen name="TelaC" component={TelaC} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
